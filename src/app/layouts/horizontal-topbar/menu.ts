5e769277b8e580f474001a6643453e5999664e19
djerfy <djerfy@gmail.com>
2024-02-01 21:54:53 +0100

    Add reward/rewards menu
    Signed-off-by: djerfy <djerfy@gmail.com>

--- src/app/layouts/horizontal-topbar/menu.ts
+++ src/app/layouts/horizontal-topbar/menu.ts
@@ -26,99 +26,105 @@ export const MENU: MenuItem[] = [
   },
   {
     id: 5,
+    label: 'MENUITEMS.REWARDS.TEXT',
+    icon: 'ri-wallet-line',
+    link: '/rewards'
+  },
+  {
+    id: 6,
     label: 'MENUITEMS.CHIA.TEXT',
     icon: 'ri-plant-line',
     link: '/chia'
   },
   {
-    id: 6,
+    id: 7,
     label: 'MENUITEMS.POOL.TEXT',
     icon: 'ri-dashboard-2-line',
     subItems: [
       {
-        id: 61,
+        id: 71,
         label: 'MENUITEMS.POOL.LIST.STATUS',
         link: '/pool-status',
-        parentId: 6
-      },
-      {
-        id: 62,
-        label: 'MENUITEMS.POOL.LIST.STATS',
-        link: '/pool-stats',
-        parentId: 6
-      }
-    ]
-  },
-  {
-    id: 7,
-    label: 'MENUITEMS.INFO.TEXT',
-    icon: 'ri-information-line',
-    subItems: [
-      {
-        id: 71,
-        label: 'MENUITEMS.INFO.LIST.FAQ',
-        link: '/info-faq',
         parentId: 7
       },
       {
         id: 72,
-        label: 'MENUITEMS.INFO.LIST.API',
-        link: '/info-api',
-        parentId: 7
-      },
-      {
-        id: 73,
-        label: 'MENUITEMS.INFO.LIST.FEE',
-        link: '/info-fee',
-        parentId: 7
-      },
-      {
-        id: 74,
-        label: 'MENUITEMS.INFO.LIST.TEAM',
-        link: '/info-team',
+        label: 'MENUITEMS.POOL.LIST.STATS',
+        link: '/pool-stats',
         parentId: 7
       }
     ]
   },
   {
     id: 8,
-    label: 'MENUITEMS.CONTACT.TEXT',
-    icon: 'ri-headphone-line',
+    label: 'MENUITEMS.INFO.TEXT',
+    icon: 'ri-information-line',
     subItems: [
       {
         id: 81,
-        label: 'MENUITEMS.CONTACT.LIST.TELEGRAM',
-        icon: 'ri-telegram-line',
-        link: 'https://t.me/chiapoolenergy',
+        label: 'MENUITEMS.INFO.LIST.FAQ',
+        link: '/info-faq',
         parentId: 8
       },
       {
         id: 82,
-        label: 'MENUITEMS.CONTACT.LIST.TWITTER',
-        icon: 'ri-twitter-line',
-        link: 'https://twitter.com/pool_energy',
+        label: 'MENUITEMS.INFO.LIST.API',
+        link: '/info-api',
         parentId: 8
       },
       {
         id: 83,
-        label: 'MENUITEMS.CONTACT.LIST.REDDIT',
-        icon: 'ri-reddit-line',
-        link: 'https://www.reddit.com/r/chia/comments/thv366/new_chia_pool_poolenergy/',
+        label: 'MENUITEMS.INFO.LIST.FEE',
+        link: '/info-fee',
         parentId: 8
       },
       {
         id: 84,
+        label: 'MENUITEMS.INFO.LIST.TEAM',
+        link: '/info-team',
+        parentId: 8
+      }
+    ]
+  },
+  {
+    id: 9,
+    label: 'MENUITEMS.CONTACT.TEXT',
+    icon: 'ri-headphone-line',
+    subItems: [
+      {
+        id: 91,
+        label: 'MENUITEMS.CONTACT.LIST.TELEGRAM',
+        icon: 'ri-telegram-line',
+        link: 'https://t.me/chiapoolenergy',
+        parentId: 9
+      },
+      {
+        id: 92,
+        label: 'MENUITEMS.CONTACT.LIST.TWITTER',
+        icon: 'ri-twitter-line',
+        link: 'https://twitter.com/pool_energy',
+        parentId: 9
+      },
+      {
+        id: 93,
+        label: 'MENUITEMS.CONTACT.LIST.REDDIT',
+        icon: 'ri-reddit-line',
+        link: 'https://www.reddit.com/r/chia/comments/thv366/new_chia_pool_poolenergy/',
+        parentId: 9
+      },
+      {
+        id: 94,
         label: 'MENUITEMS.CONTACT.LIST.YOUTUBE',
         icon: 'ri-youtube-line',
         link: 'https://www.youtube.com/channel/UCCHIIBGO-PA-UfxJDlcz0aw',
-        parentId: 8
+        parentId: 9
       },
       {
-        id: 85,
+        id: 95,
         label: 'MENUITEMS.CONTACT.LIST.FACEBOOK',
         icon: 'ri-facebook-line',
         link: 'https://www.facebook.com/pool.energy/',
-        parentId: 8
+        parentId: 9
       }
     ]
   }
