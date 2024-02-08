b44a5a430443c95a5922a050bd66c994700ce919
djerfy <djerfy@gmail.com>
2024-02-08 15:59:40 +0100

    feat: add blocks chart
    Signed-off-by: djerfy <djerfy@gmail.com>

--- src/app/pages/views/farmer/farmer.component.ts
+++ src/app/pages/views/farmer/farmer.component.ts
@@ -21,6 +21,9 @@ export class FarmerComponent {
   blocksPageSize: number = 15;
   blocksEffortCount: number = 0;
   blocksEffortAverage: number = 0;
+  blocksPerDayChart: any = {};
+  blocksPerDayChartLegend: boolean = false;
+  blocksPerDayChartData: any[] = [];
 
   launcher_id: any = null;
   launcher: any = {};
@@ -66,6 +69,67 @@ export class FarmerComponent {
     this.blocksEffortCount = blocksCountFromList;
     this.blocksCollectionSize = data['count'];
     this._blocks$.next(data['results']);
+    this.chartBlocks(data);
+  }
+
+  private getChartColorsArray(colors:any) {
+    colors = JSON.parse(colors);
+    return colors.map(function (value:any) {
+      var newValue = value.replace(" ", "");
+      if (newValue.indexOf(",") === -1) {
+        var color = getComputedStyle(document.documentElement).getPropertyValue(newValue);
+            if (color) {
+            color = color.replace(" ", "");
+            return color;
+            }
+            else return newValue;;
+        } else {
+            var val = value.split(',');
+            if (val.length == 2) {
+                var rgbaColor = getComputedStyle(document.documentElement).getPropertyValue(val[0]);
+                rgbaColor = "rgba(" + rgbaColor + "," + val[1] + ")";
+                return rgbaColor;
+            } else {
+                return newValue;
+            }
+        }
+    });
+  }
+
+  private chartBlocks(data: any) {
+    var blocksPerDay: Map<String, number> = new Map();
+    (<any[]>data['results']).map((item) => {
+      var date = new Date(Math.floor(item['timestamp']) * 1000).toLocaleDateString();
+      blocksPerDay.set(date, (blocksPerDay.get(date) || 0) + 1);
+    });
+    var seriesBlocks: any = [];
+    blocksPerDay.forEach((v, k) => {
+      seriesBlocks.push({"x": k, "y": v})
+    });
+
+    this.blocksPerDayChart = {
+      series: [{
+        name: "Block(s)",
+        data: seriesBlocks.reverse(),
+      }],
+      legend: {
+        show: this.blocksPerDayChartLegend
+      },
+      chart: {
+        height: 250,
+        type: "bar",
+        toolbar: {
+          show: false
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        formatter: function(val: any) {
+          return val;
+        }
+      },
+      colors: this.getChartColorsArray('["--vz-primary"]')
+    };
   }
 
   humanize(seconds: number) {
