5ef4c3d0f7b32a9fc398f15c001976828896a88e
djerfy <djerfy@gmail.com>
2024-03-29 16:25:36 +0100

    misc: improve join/settings pages
    Signed-off-by: djerfy <djerfy@gmail.com>

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -14,6 +14,7 @@ import { InfoApiComponent } from './info-api/info-api.component';
 import { InfoFeeComponent } from './info-fee/info-fee.component';
 import { InfoTeamComponent } from './info-team/info-team.component';
 import { LoginComponent } from './login/login.component';
+import { JoinComponent } from './join/join.component';
 
 const routes: Routes = [
   { path: '', component: FarmersComponent },
@@ -30,7 +31,8 @@ const routes: Routes = [
   { path: 'info/fee', component: InfoFeeComponent },
   { path: 'info/team', component: InfoTeamComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'settings', component: LoginComponent }
+  { path: 'settings', component: LoginComponent },
+  { path: 'join', component: JoinComponent }
 ];
 
 @NgModule({
