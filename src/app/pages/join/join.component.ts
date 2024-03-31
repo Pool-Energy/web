9170d35d02d718faddf249453104d5f7a95ee85e
djerfy <djerfy@gmail.com>
2024-03-31 13:30:32 +0200

    feat: pool live latency
    Signed-off-by: djerfy <djerfy@gmail.com>

--- src/app/pages/join/join.component.ts
+++ src/app/pages/join/join.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject, interval } from 'rxjs';
+
 
 @Component({
   selector: 'app-join',
@@ -9,13 +12,36 @@ import { Component } from '@angular/core';
 export class JoinComponent {
   breadCrumbItems!: Array<{}>;
 
-  constructor() { }
+  // latency
+  pingStream: Subject<number> = new Subject<number>();
+  pingPoolLatency: number = 0;
+  pingPoolInterval: number = 3000;
+  pingPoolUrl: string = 'https://chia.pool.energy';
+
+  constructor(
+    private httpClient: HttpClient
+  ) {}
 
   ngOnInit(): void {
     this.breadCrumbItems = [
       { label: 'Pool' },
       { label: 'Join', active: true }
     ];
+
+    // latency
+    this.getPoolLatency(this.pingPoolUrl, this.pingPoolInterval);
+  }
+
+  getPoolLatency(pingUrl: string, pingInterval: number) {
+    interval(pingInterval).subscribe(() => {
+      let timeStart: number = performance.now();
+      this.httpClient.get(pingUrl, {observe:'response', responseType:'text'}).subscribe(() => {
+        let timeEnd: number = performance.now();
+        let ping: number = timeEnd - timeStart;
+        this.pingPoolLatency = ping;
+        this.pingStream.next(ping);
+      });
+    });
   }
 
 }
