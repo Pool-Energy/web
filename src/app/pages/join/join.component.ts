5bf7f12742e5eac32e320ad607c5d22e262b685d
djerfy <djerfy@gmail.com>
2024-03-31 14:52:21 +0200

    feat: add ping (each) chart
    Signed-off-by: djerfy <djerfy@gmail.com>

--- src/app/pages/join/join.component.ts
+++ src/app/pages/join/join.component.ts
@@ -12,8 +12,12 @@ export class JoinComponent {
   breadCrumbItems!: Array<{}>;
 
   // latency
-  pingPoolLatency: number = 0;
   pingPoolUrl: string = 'https://chia.pool.energy';
+  pingPoolLatencyArray: number[] = [];
+  pingPoolLatencyAverage: number = 0;
+  pingPoolLatencyChart: any = {};
+  pingPoolLatencyChartLegend: boolean = false;
+  pingPoolLatencyChartData: any[] = [];
 
   constructor(
     private httpClient: HttpClient
@@ -29,19 +33,71 @@ export class JoinComponent {
     this.getPingLatency(this.pingPoolUrl);
   }
 
+  // common
+  private getChartColorsArray(colors:any) {
+    colors = JSON.parse(colors);
+    return colors.map(function (value:any) {
+      var newValue = value.replace(" ", "");
+      if (newValue.indexOf(",") === -1) {
+        var color = getComputedStyle(document.documentElement).getPropertyValue(newValue);
+            if (color) {
+            color = color.replace(" ", "");
+            return color;
+            }
+            else return newValue;;
+        } else {
+            var val = value.split(',');
+            if (val.length == 2) {
+                var rgbaColor = getComputedStyle(document.documentElement).getPropertyValue(val[0]);
+                rgbaColor = "rgba(" + rgbaColor + "," + val[1] + ")";
+                return rgbaColor;
+            } else {
+                return newValue;
+            }
+        }
+    });
+  }
+
+  // latency
   getPingLatency(pingUrl: string) {
-    let arr: number[] = [];
-    for(let i = 0; i < 3; i++) {
+    for(let i = 0; i < 5; i++) {
       setTimeout(() => {
         let timeStart: number = performance.now();
         this.httpClient.get(pingUrl, {observe:'response', responseType:'text'}).subscribe(() => {
           let timeEnd: number = performance.now();
           let ping: number = timeEnd - timeStart;
-          arr.push(ping);
-          this.pingPoolLatency = arr.reduce((a, b) => a + b, 0) / arr.length;
+          this.pingPoolLatencyArray.push(ping);
+          this.pingPoolLatencyAverage = this.pingPoolLatencyArray.reduce((a, b) => a + b, 0) / this.pingPoolLatencyArray.length;
         });
       }, 2000);
     }
+    this.chartPingLatency(this.pingPoolLatencyArray);
+  }
+
+  private chartPingLatency(data: any) {
+    this.pingPoolLatencyChart = {
+      series: [{
+        name: "Ping",
+        data: this.pingPoolLatencyArray,
+      }],
+      legend: {
+        show: this.pingPoolLatencyChartLegend
+      },
+      chart: {
+        height: 250,
+        type: "area",
+        toolbar: {
+          show: false
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        formatter: function(val: any) {
+          return val + " ms";
+        }
+      },
+      colors: this.getChartColorsArray('["--vz-primary"]')
+    };
   }
 
 }
