b650143ff76d36474180d7814260021733504132
djerfy <djerfy@gmail.com>
2025-01-08 20:46:30 +0100

    feat: review of pool status page
    Signed-off-by: djerfy <djerfy@gmail.com>

--- src/app/pages/pool-status/pool-status.component.ts
+++ src/app/pages/pool-status/pool-status.component.ts
@@ -16,14 +16,7 @@ export class PoolStatusComponent {
   blockchainShare: number = 0;
 
   // wallet
-  walletAddressTruncate: boolean = false;
   pool_wallets: Array<any> = new Array();
-
-  // mempool size
-  mempoolSizeDays: number = 7;
-  mempoolSizeData: any[] = [];
-  mempoolSizeChart: any = {};
-  mempoolSizeChartLegend: boolean = false;
 
   constructor(
     private dataService: DataService
@@ -36,106 +29,16 @@ export class PoolStatusComponent {
       { label: 'Status', active: true }
     ];
 
-    this.walletAddressTruncate = window.innerWidth <= 2350;
-    window.onresize = () => this.walletAddressTruncate = window.innerWidth <= 2350;
-
     this.dataService.getStats().subscribe((data: any) => {
       this.blockchainHeight = data['blockchain_height'];
       this.blockchainShare = this.getBlockchainShare(data);
       this.pool_wallets = data['pool_wallets'];
-    });
-    this.getMempoolSize(this.mempoolSizeDays);
-  }
-
-  // common
-  private getChartColorsArray(colors: any) {
-    colors = JSON.parse(colors);
-    return colors.map(function (value:any) {
-      var newValue = value.replace(" ", "");
-      if (newValue.indexOf(",") === -1) {
-        var color = getComputedStyle(document.documentElement).getPropertyValue(newValue);
-          if (color) {
-            color = color.replace(" ", "");
-            return color;
-          } else {
-            return newValue;
-          }
-        } else {
-            var val = value.split(',');
-            if (val.length == 2) {
-              var rgbaColor = getComputedStyle(document.documentElement).getPropertyValue(val[0]);
-              rgbaColor = "rgba(" + rgbaColor + "," + val[1] + ")";
-              return rgbaColor;
-            } else {
-              return newValue;
-            }
-        }
     });
   }
 
   // blockchain
   getBlockchainShare(data: any) {
     return data['pool_space'] * 100 / data['blockchain_space'];
-  }
-
-  // mempool size
-  getMempoolSize(days: number) {
-    this.dataService.getMempool(days).subscribe((d: any) => {
-      this.mempoolSizeDays = days;
-      this.mempoolSizeData = [{
-        "name": "Full Percentage (%)",
-        "data": (<any[]>d).filter(item => item['field'] == 'full_pct').map((item) => {
-          return ({
-            "x": (new Date(item['datetime']).toLocaleString()),
-            "y": item['value'],
-          })
-        })
-      }];
-      this.chartMempoolSize(this.mempoolSizeData);
-    })
-  }
-
-  private chartMempoolSize(data: any) {
-    this.mempoolSizeChart = {
-      series: data,
-      legend: {
-        show: this.mempoolSizeChartLegend
-      },
-      chart: {
-        height: 350,
-        type: "area",
-        toolbar: {
-          show: false
-        },
-        zoom: {
-          enabled: true
-        }
-      },
-      dataLabels: {
-        enabled: false
-      },
-      noData: {
-        text: "Loading..."
-      },
-      xaxis: {
-        type: 'datetime',
-        labels: {
-          show: false
-        }
-      },
-      yaxis: {
-        min: 0,
-        labels: {
-          formatter: function (val: number) {
-            return val.toFixed(2) + " %";
-          }
-        }
-      },
-      stroke: {
-        width: 2
-      },
-      colors: this.getChartColorsArray('["--vz-success"]')
-    }
   }
 
 }
