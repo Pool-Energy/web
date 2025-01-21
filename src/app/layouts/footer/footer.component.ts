e5c89848a31e739b207d210a072f0780c0c3fc35
djerfy <djerfy@gmail.com>
2025-01-21 21:20:15 +0100

    feat: rename twitter to x
    Signed-off-by: djerfy <djerfy@gmail.com>

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -11,7 +11,7 @@ export class FooterComponent implements OnInit {
 
   links = {
     "telegram": "https://t.me/chiapoolenergy",
-    "twitter": "https://twitter.com/pool_energy",
+    "xing": "https://x.com/pool_energy",
     "reddit": "https://www.reddit.com/r/chia/comments/thv366/new_chia_pool_poolenergy/",
     "youtube": "https://www.youtube.com/channel/UCCHIIBGO-PA-UfxJDlcz0aw",
     "facebook": "https://www.facebook.com/pool.energy/",
